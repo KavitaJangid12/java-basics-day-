eeec4b603cb0b673fb7e77b8b96abd9c863de98e
KavitaJangid12 <kavitajangid1205@gmail.com>
2025-12-16 10:06:16 +0530

    Variable Bugs Detection and Correction

---
+++ VariablesBug.java
@@ -0,0 +1,17 @@
+public class VariablesBug {
+    public static void main(String[] args) {
+//        int number = '10';
+//        String Name = Kavita;
+//        long -mobile = 9866;
+//        System.out.println(number);
+//        System.out.println(Name)
+//                System.out.println("mobile");
+        //Corrections:
+        int number = 10;
+        String name = "Kavita";
+        float mobile = 9866f;
+        System.out.println(number);
+        System.out.println(name);
+        System.out.println(mobile);
+    }
+}
